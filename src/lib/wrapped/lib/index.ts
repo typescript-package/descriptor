393952ed6bf723b303f9a0b542318a79aa202714
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-08-03 23:58:09 +0000

    chore(wrapped/index): update.

--- src/lib/wrapped/lib/index.ts
+++ src/lib/wrapped/lib/index.ts
@@ -1,3 +1,7 @@
-export { WrappedDescriptor } from './wrapped-descriptor.class';
+// Abstract.
+export { PlainWrappedDescriptorBase } from './plain-wrapped-descriptor-base.abstract';
 export { WrappedDescriptorBase } from './wrapped-descriptor-base.abstract';
+// Class.
+export { PlainWrappedDescriptor } from './plain-wrapped-descriptor.class';
+export { WrappedDescriptor } from './wrapped-descriptor.class';
 export { WrappedDescriptorCore } from './wrapped-descriptor-core.abstract';
