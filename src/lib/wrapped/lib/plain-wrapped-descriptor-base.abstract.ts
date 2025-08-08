cbae17cff8d3fc340804e5b1056c4a8b2b7683a0
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-08-08 09:22:56 +0000

    refactor(`PlainWrappedDescriptorBase`): remove the static properties.

--- src/lib/wrapped/lib/plain-wrapped-descriptor-base.abstract.ts
+++ src/lib/wrapped/lib/plain-wrapped-descriptor-base.abstract.ts
@@ -38,22 +38,6 @@ export abstract class PlainWrappedDescriptorBase<
   D extends PlainWrappedDescriptorBase<O, K, V, A, N, C, E, D> = PlainWrappedDescriptorBase<O, K, V, A, N, C, E, any>,
 > extends WrappedDescriptorCore<O, K, V, A, N, C, E, D> {
   /**
-   * @description The defaults for instance `active` property.
-   * @public
-   * @static
-   * @type {boolean}
-   */
-  public static active: boolean = true;
-
-  /**
-   * @description The defaults for instance `enabled` property.
-   * @public
-   * @static
-   * @type {boolean}
-   */
-  public static enabled: boolean = true;
-
-  /**
    * @description The active state of the descriptor.
    * @public
    * @type {(A | { onGet?: boolean; onSet?: boolean })}
@@ -77,9 +61,9 @@ export abstract class PlainWrappedDescriptorBase<
   /**
    * @description The optional `get` method for the descriptor.
    * @public
-   * @type {?(this: O, descriptor?: D) => V}
+   * @type {(this: O, descriptor?: D) => V}
    */
-  public get?: (this: O, descriptor?: D) => V;
+  public get: (this: O, descriptor?: D) => V;
   
   /**
    * @description The key of the descriptor.
@@ -107,7 +91,7 @@ export abstract class PlainWrappedDescriptorBase<
    * @public
    * @type {?'value' extends keyof D ? PropertyDescriptor : D}
    */
-  public previousDescriptor?: 'value' extends keyof D ? PropertyDescriptor : D;
+  public previousDescriptor: 'value' extends keyof D ? PropertyDescriptor : D;
 
   /**
    * @description The private key for the descriptor.
@@ -119,40 +103,46 @@ export abstract class PlainWrappedDescriptorBase<
   /**
    * @description The optional `set` method for the descriptor.
    * @public
-   * @type {?(this: O, value: V, descriptor?: D) => void}
+   * @type {(this: O, value: V, descriptor?: D) => void}
    */
-  public set?: (this: O, value: V, descriptor?: D) => void;
+  public set: (this: O, value: V, descriptor?: D) => void;
 
   /**
    * Creates an instance of `WrappedDescriptorBase` child class.
    * @constructor
    * @param {O} object The object to define the descriptor on.
    * @param {K} key The key of the object to define the descriptor on.
-   * @param {WrappedPropertyDescriptor<O, K, V, A, N, C, E, D>} descriptor The property descriptor to wrap.
+   * @param {Partial<WrappedPropertyDescriptor<O, K, V, A, N, C, E, D>>} attributes The property descriptor to wrap.
    */
   constructor(
     object: O,
     key: K, 
-    descriptor: WrappedPropertyDescriptor<O, K, V, A, N, C, E, D>,
+    attributes: Partial<WrappedPropertyDescriptor<O, K, V, A, N, C, E, D>>,
   ) {
-    super(descriptor);
+    super(attributes);
 
     delete this.index;
     delete this.onGet;
     delete this.onSet;
-    delete this.previousDescriptor;
+    // delete this.previousDescriptor;
 
     // Assign the properties.
     // Required.
-    this.active = typeof descriptor.active === 'boolean' || typeof descriptor.active === 'object' ? descriptor.active : PlainWrappedDescriptorBase.active as A;
-    this.enabled = typeof descriptor.enabled === 'boolean' || typeof descriptor.enabled === 'object' ? descriptor.enabled : PlainWrappedDescriptorBase.enabled as N;
+    this.active = typeof attributes.active === 'boolean' || typeof attributes.active === 'object' ? attributes.active : PlainWrappedDescriptorBase.active as A;
+    this.enabled = typeof attributes.enabled === 'boolean' || typeof attributes.enabled === 'object' ? attributes.enabled : PlainWrappedDescriptorBase.enabled as N;
     this.key = key;
-    this.previousDescriptor = (descriptor.previousDescriptor || Object.getOwnPropertyDescriptor(object, key)) as any;
-    this.privateKey = descriptor.privateKey || `_${String(key)}`;
+    this.previousDescriptor = (attributes.previousDescriptor || Object.getOwnPropertyDescriptor(object, key)) as any;
+    this.privateKey = attributes.privateKey || `_${String(key)}`;
 
     // Optional.
-    descriptor.index && (this.index = descriptor.index);
-    descriptor.onGet && (this.onGet = descriptor.onGet);
-    descriptor.onSet && (this.onSet = descriptor.onSet);
+    attributes.index && (this.index = attributes.index);
+    attributes.onGet && (this.onGet = attributes.onGet);
+    attributes.onSet && (this.onSet = attributes.onSet);
+
+    // Wraps the `get` getter and `set` setter  of the descriptor.
+    const {get, set} = super.wrap(attributes);
+    // Assigns the wrapped `get` getter and `set` setter to the descriptor.
+    this.get = get!
+    this.set = set!;
   }
 }
