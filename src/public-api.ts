16488c8b96faa45c045b1dfd3f7f4ae68d0fcb6b
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-08-14 13:53:13 +0000

    chore(api): update.

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,18 +1,12 @@
 /*
  * Public API Surface of descriptor
  */
+
 export {
   // Abstract.
   CommonDescriptor,
-  DescriptorChainCore,
-  WrappedDescriptorCore,
-
   // Class.
   AccessorDescriptor,
   DataDescriptor,
-  Descriptor,
-  DescriptorChain,
-  Descriptors,
-  WeakWrappedDescriptor,
-  WrappedDescriptor
+  Descriptor
 } from './lib';
